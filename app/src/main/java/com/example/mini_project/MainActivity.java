9993cefba835e149dfd9e44c2f1876946e5a8464
younes <elhakimyyouness4@gmail.com>
2023-06-11 22:48:25 +0100

    OnClickListener for Multiple Views

--- app/src/main/java/com/example/mini_project/MainActivity.java
+++ app/src/main/java/com/example/mini_project/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.mini_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button loadusers, quit;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadusers = findViewById(R.id.loaduser);
+        quit = findViewById(R.id.quit);
+
+        loadusers.setOnClickListener(this);
+        quit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == loadusers){
+            Toast.makeText(this, "Loud Users", Toast.LENGTH_SHORT).show();
+        } else if (v.getId() == R.id.quit) {
+            finish();
+        }
     }
 }
